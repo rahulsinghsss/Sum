d42da609b3044c50cadc307654baab13012d2faf
MahonriM <cripto3000z@gmail.com>
2018-10-26 14:39:16 -0500

    Suma
    Suma de 2 numeros en Python 3.0
    Hecho por Mahonri M

---
+++ suma.py
@@ -0,0 +1,7 @@
+#Suma de dos numeros
+#Entrada de datos
+x=input ('Ingresa el primer numero')
+y=input('Ingresa el primer numero')
+#Conversion de tipos de datos
+s=int(x)+int(y)
+print("La suma es", s)
